7f911a64e4427b3c0c49515796051603f7aa9eb8
brunokiafuka <brunokiafuka@live.com.pt>
2020-07-03 09:59:55 +0200

    :sparkles: new component
    friends list

---
+++ src/components/Lists/FriendsList.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import AddNew from "../Buttons/AddNew";
+import Avatar from "../Avatars/Menu";
+import Colors from "../../constants/Colors";
+import { User } from "../../helpers/interfaces";
+
+interface Props {
+  list: Array<User>;
+}
+
+const FriendsList: React.FC<Props> = ({ list }) => {
+  return (
+    <View>
+      <FlatList
+        contentContainerStyle={{ alignItems: "center", paddingLeft: 13 }}
+        data={list}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item, index }) => (
+          <>
+            {index === 0 ? (
+              <AddNew key={item.fullName} onPress={() => {}} />
+            ) : (
+              <TouchableOpacity
+                key={item.id}
+                style={{
+                  backgroundColor: Colors.offWhite,
+                  marginHorizontal: 10,
+                  justifyContent: "center",
+                  alignItems: "center",
+                  paddingHorizontal: 10,
+                  paddingVertical: 20,
+                  borderRadius: 8,
+                }}
+              >
+                <Avatar uri={item.avatar} />
+                <Text style={{ marginTop: 10, color: Colors.darkBlue }}>
+                  {item.fullName}
+                </Text>
+              </TouchableOpacity>
+            )}
+          </>
+        )}
+      />
+    </View>
+  );
+};
+
+export default FriendsList;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import * as Buttons from "./Buttons";
 import * as Avatars from "./Avatars";
 import * as Separators from "./Separators";
+import * as Lists from "./Lists";
 
-export { Buttons, Avatars, Separators };
+export { Buttons, Avatars, Separators, Lists };
